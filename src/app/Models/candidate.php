cca803058dfd4d7dc385e633c7264bb8aae10c2b
Vzrhkv <lieden57@gmail.com>
2023-05-24 22:02:27 +0300

    updated candidate model

--- src/app/Models/candidate.php
+++ src/app/Models/candidate.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Candidate extends Model
 {
@@ -13,4 +14,9 @@ class Candidate extends Model
     protected $fillable = [
         'email', 'password'
     ];
+
+    public function candidateFullName(): HasOne
+    {
+        return $this->hasOne(candidate_full_name::class);
+    }
 }
