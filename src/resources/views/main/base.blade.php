8bcf05d7cd7bba8f238e166270bbb0607eaee2f2
Vzrhkv <lieden57@gmail.com>
2023-06-01 22:08:50 +0300

    added organization_list

--- src/resources/views/main/base.blade.php
+++ src/resources/views/main/base.blade.php
@@ -10,7 +10,7 @@
         href="https://fonts.googleapis.com/css2?family=Montserrat&family=Work+Sans:wght@400;500;600;700;900&display=swap"
         rel="stylesheet">
 
-{{--    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />--}}
+   <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
 
     @stack('styles')
 
